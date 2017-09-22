64648b47a586f11dab6a299f9a1cfcdbc96568f2
vikihunk <vikihunk@gmail.com>
2017-09-22 11:13:02 +0530

    Added delete in the method list

--- DS/LinkedList/my_list.py
+++ DS/LinkedList/my_list.py
@@ -14,6 +14,14 @@ class linked_list():
 			cur = cur.next
 		cur.next = new_node
 
+	def length(self):
+		cur = self.head
+		total = 0
+		while cur.next != None:
+			cur = cur.next
+			total += 1	
+		return total
+
 	def display(self):
 		elems = []
 		cur = self.head
@@ -22,10 +30,31 @@ class linked_list():
 			elems.append(cur.data)
 		print elems
 
+	def delete(self, index):
+		idx = 0
+		if index >= self.length():
+			print "Error: index out of range"
+			return False
+		cur = self.head
+		while True:
+			last_node = cur
+			cur = cur.next
+			if idx == index:
+				last_node.next = cur.next
+				return True
+			idx += 1
+		return False	
+			
 my_list = linked_list()
 my_list.display()
+
 my_list.append(0)
 my_list.append(1)
 my_list.append(2)
 my_list.append(3)
+
+my_list.display()
+
+my_list.delete(2)
+
 my_list.display()
